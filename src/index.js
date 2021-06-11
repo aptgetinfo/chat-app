9544adea1f3046c8b8f36bc6fdee377216077ccd
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-06-12 04:00:20 +0530

    location added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path =require('path')
 const http =require('http')
 const express =require('express')
 const socketio=require('socket.io')
+const { countReset } = require('console')
 
 const app=express()
 const server=http.createServer(app)
@@ -13,9 +14,20 @@ const port =process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
 
-
-io.on('connection',()=>{
+io.on('connection',(socket)=>{
     console.log('New WebSocket Connection')
+    socket.emit('message','Welcome')
+    socket.broadcast.emit('message','A new user has joined !')
+
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',message)
+    })
+    socket.on('sendLocation',(coords)=>{
+        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+    socket.on('disconnect',()=>{
+        io.emit('message','User has left')
+    })
 })
 
 
