baf182f427a2dce4a146f50e88e2ce5252115a4a
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-06-12 09:08:36 +0530

    auto-scroll added

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,6 +13,31 @@ const sidebarTempelate=document.querySelector('#sidebar-tempelate').innerHTML
 
 //options
 const {username,room}=Qs.parse(location.search,{ignoreQueryPrefix:true})
+const autoscroll=()=>{
+    //new message element
+    const $newMessage=$messages.lastElementChild
+
+    //height of the new message
+    const newMessageStyles=getComputedStyle($newMessage)
+    const newMessageMargin=parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight=$newMessage.offsetHeight+newMessageMargin
+
+
+    //visible height
+    const visibleHeight=$messages.offsetHeight
+
+    //height of msg container
+
+    const containerHeight=$messages.scrollHeight
+
+    //how far have i scrolled?
+    const scrollOffset=$messages.scrollTop+visibleHeight
+
+
+    if(containerHeight-newMessageHeight<=scrollOffset){
+        $messages.scrollTop=$messages.scrollHeight
+    }
+}
 
 
 
@@ -25,6 +50,7 @@ socket.on('message',(message)=>{
 
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
 })
 socket.on('locationMessage',(message)=>{
     // console.log(url)
@@ -34,6 +60,7 @@ socket.on('locationMessage',(message)=>{
         createdAt:moment(message.createdAt).format('h: mm A')
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
 })
 
 socket.on('roomData',({room,users})=>{
